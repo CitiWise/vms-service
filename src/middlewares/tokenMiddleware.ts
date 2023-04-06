458ab0e9625ef4f304af549de585d4322e225cdd
aditya3wh <aditya.banotra@wherehouse.io>
2023-04-06 20:13:48 +0530

    temporarily remove token handler

--- src/middlewares/tokenMiddleware.ts
+++ src/middlewares/tokenMiddleware.ts
@@ -22,63 +22,7 @@ export async function tokenHandler(
   next: NextFunction
 ) {
   try {
-    // making cookieName for different panel and getting cookie(Token) from different panel so cookie name will be different
-    // So no cookie(Token) will be re initialized or re assigned
-    console.log(req.headers.apptype);
-    const cookieName = getCookieName(req.headers.apptype);
-    const token = req.signedCookies[cookieName];
-    console.log(req.signedCookies);
-    // console.log(token);
-    if (token) {
-    //   const entityId = await verifyToken(token);
-    //   logger.info(`!!!!!!!!!!!!entityId!!!!!!! ${JSON.stringify(entityId)}`);
-
-    //   /**
-    //    * checking if entityId's userType is not matching with current userType
-    //    * This userType only check once when user is logged in and call set cookie route to just set oms cookie
-    //    * If it does not match then generate token once again set oms cookie (First Case)
-    //    * If it is matched then just need to pass data (Second Case)
-    //    */
-
-    //   if (!entityId) {
-    //     throw new Error("Token is invalid , Logout user");
-    //   }
-
-      // if (req.query?.userType === entityId?.userType || entityId) {
-      //   // First Case
-      //   req[`user`] = { id: entityId.id, userType: entityId.userType };
-      //   return next();
-      // } else {
-      //   // Second Case
-
-      //   const authToken = await verifyOauthCode(String(req.query.oauthCode));
-
-      //   if (!authToken) {
-      //     throw new Error("oauth not valid , Logout user");
-      //   } else {
-      //     res.cookie(cookieName, authToken.token, defaultCookieOptions);
-      //     req[`user`] = {
-      //       id: authToken.entityId,
-      //       userType: authToken.userType,
-      //     };
-      //     return next();
-      //   }
-      // }
-      return next();
-    } else if (req.query?.oauthCode) {
-      const authToken = await verifyOauthCode(String(req.query.oauthCode));
-
-      if (!authToken) {
-        throw new Error(`oauth not valid , Logout user`);
-      } else {
-        res.cookie(cookieName, authToken.token, defaultCookieOptions);
-        req[`user`] = { id: authToken.entityId, userType: authToken.userType };
-        return next();
-      }
-    } 
-    else {
-      throw new Error(`No Token Present or Auth code , Logout user`);
-    }
+    return next();
   } catch (error: any) {
     return res.status(403).json({
       responseCode: "000028",
